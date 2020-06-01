8d1dfaf27043ec6117248c6e0393491abc7989eb
Marko Man <darkobas@gmail.com>
2020-06-01 16:33:14 +0200

    op7tpro: back to 1.0 fod
    Change-Id: I4e86bef2f86862443470e336ba9b7fa4802eefc0

--- device.mk
+++ device.mk
@@ -28,7 +28,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += fstab.qcom
 
 PRODUCT_PACKAGES += \
-    omni.biometrics.fingerprint.inscreen@1.1-service.oneplus7pro
+    omni.biometrics.fingerprint.inscreen@1.0-service.oneplus7pro
 
 
 $(call inherit-product, device/oneplus/oneplus7pro/device.mk)
