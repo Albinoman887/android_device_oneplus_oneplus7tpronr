Albinoman887/android_device_oneplus_oneplus7tpro

overlayfs: Fix booting with SELinux set as permissive
* Seems the mount point "checkpoint=block" boot hang when in SELinux
  permissive mode. import modified fstab accordingly and override the
  one in /vendor with overlayfs.

Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
Change-Id: I985e6020fcc9ecc58b8ae7d337956640b8b2ffb4

device.mk
@@ -23,7 +23,8 @@
 # Prebuilt
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,device/oneplus/oneplus7pro/prebuilt/product,product) \
-    $(call find-copy-subdir-files,*,device/oneplus/oneplus7pro/prebuilt/root,recovery/root)
+    $(call find-copy-subdir-files,*,device/oneplus/oneplus7pro/prebuilt/root,recovery/root) \
+    $(call find-copy-subdir-files,*,device/oneplus/oneplus7tpro5g/prebuilt/product,product) \
 
 PRODUCT_PACKAGES += fstab.qcom
 

Albinoman887/android_device_oneplus_oneplus7tpronr

device.mk
@@ -23,7 +23,8 @@
 # Prebuilt
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,device/oneplus/oneplus7pro/prebuilt/product,product) \
-    $(call find-copy-subdir-files,*,device/oneplus/oneplus7pro/prebuilt/root,recovery/root)
+    $(call find-copy-subdir-files,*,device/oneplus/oneplus7pro/prebuilt/root,recovery/root) \
+    $(call find-copy-subdir-files,*,device/oneplus/oneplus7tpronr/prebuilt/product,product) \
 
 PRODUCT_PACKAGES += fstab.qcom
 